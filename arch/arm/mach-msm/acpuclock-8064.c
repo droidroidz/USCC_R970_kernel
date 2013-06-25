14e4e512ff9728b18103ad244bec2fc20e63f313
droidroidz <friesk@mchsi.com>
2013-06-25 13:48:49 -0500

    mach-msm: acpuclock: lower bus freq add L2 steps

--- arch/arm/mach-msm/acpuclock-8064.c
+++ arch/arm/mach-msm/acpuclock-8064.c
@@ -107,7 +107,7 @@ static struct msm_bus_paths bw_level_tbl[] __initdata = {
 	[3] = BW_MBPS(2128), /* At least 266 MHz on bus. */
 	[4] = BW_MBPS(3200), /* At least 400 MHz on bus. */
 	[5] = BW_MBPS(4264), /* At least 533 MHz on bus. */
-	[6] = BW_MBPS(4800), /* At least 600 MHz on bus. */
+	[6] = BW_MBPS(4600), /* At least 600 MHz on bus. */
 	
 };
 
@@ -135,6 +135,8 @@ static struct l2_level l2_freq_tbl[] __initdata = {
 	[13] = { { 1080000, HFPLL, 1, 0x28 }, 1150000, 1150000, 5 },
 	[14] = { { 1134000, HFPLL, 1, 0x2A }, 1150000, 1150000, 5 },
 	[15] = { { 1188000, HFPLL, 1, 0x2C }, 1150000, 1150000, 5 },
+	[16] = { { 1242000, HFPLL, 1, 0x2E }, 1250000, 1250000, 6 },
+	[17] = { { 1296000, HFPLL, 1, 0x30 }, 1250000, 1250000, 6 },
 	{ }
 };
 
