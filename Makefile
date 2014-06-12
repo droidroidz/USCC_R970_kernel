94035fa6f3062afab9d67e2873449937f1d6ecc8
droidroidz <friesk@mchsi.com>
2014-06-12 15:32:08 -0500

    linux 3.4.92

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 VERSION = 3
 PATCHLEVEL = 4
-SUBLEVEL = 91
+SUBLEVEL = 92
 EXTRAVERSION = -v0.8.6
 NAME = Saber-toothed Squirrel
 
--- fs/ecryptfs/keystore.c
+++ fs/ecryptfs/keystore.c
@@ -1204,6 +1204,7 @@ decrypt_pki_encrypted_session_key(struct ecryptfs_auth_tok *auth_tok,
 out:
 	if (msg)
 		kfree(msg);
+	kfree(payload);
 	if (auth_tok_sig)
         kfree(auth_tok_sig);
 	if (payload)
